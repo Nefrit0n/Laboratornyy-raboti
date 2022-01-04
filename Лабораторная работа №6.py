b7c95ae5873bbf9a327fb8ba3d9561fd8e8238f3
Nefrit0n <64070252+Nefrit0n@users.noreply.github.com>
2022-01-04 14:38:16 +0400

    лаба

--- Лабораторная работа №6.py
+++ Лабораторная работа №6.py
@@ -8,15 +8,30 @@ count = 0
 def dobavit_slovo(slovo_1):  # добавление слова
     global count
     global x
+    global lst_1
+    global lst_2
     c = hash_function(slovo_1)
     if lst_2[c] == '':
         lst_2[c] = c
         count += 1
     lst_1[c].append(slovo_1)
     if count == x / 2:
-        for i in range(x)
-            lst_1.append([])
-            lst_2.append('')
+        lst_3 = []
+        for j in range(x):
+            if lst_1[j] != '':
+                lst_3.extend(lst_1[j])
+        print(lst_3)
+        lst_1.clear()
+        lst_2.clear()
+        lst_1 = [[] for i in range(x * 2)]
+        lst_2 = ['' for i in range(x * 2)]
+        count = 0
+        for i_1 in range(len(lst_3)):
+            c = hash_function(lst_3[i_1])
+            if lst_2[c] == '':
+                lst_2[c] = c
+                count += 1
+            lst_1[c].append(lst_3[i_1])
         x = len(lst_1)
     print(lst_1)
 
@@ -30,8 +45,21 @@ def hash_function(slovo):  # рассчёт хэш функции
     return h
 
 
+def find_slovo(slovo_4):# для функции вывода слова и удаления по слову
+    for i in range(x):
+        for j in range(len(lst_1[i])):
+            if slovo_4 == lst_1[i][j]:
+                return i
+
+
 def vivesti_slovo(num):
-    print(lst_2[num], ':', lst_1[num])
+    if num == 1:
+        key = int(input('Ключик который будем выводить - '))
+        print(lst_2[key], ':', *lst_1[key])
+    elif num == 2:
+        slovo_3 = input('Слово которое ищем  - ')
+        poisk = find_slovo(slovo_3)
+        print(lst_2[poisk], ':', *lst_1[poisk])
 
 
 def vivesti_vsu_tabel():
@@ -51,8 +79,10 @@ def deleat_slovo(number):
         count -= 1
     elif number == 2:
         slovo_3 = input('слово - ')
-        lst_2[lst_1.index(slovo_3)] = ''
-        lst_1[lst_1.index(slovo_3)] = []
+        x_4 = find_slovo(slovo_3)
+        print(x_4)
+        lst_2[x_4] = ''
+        lst_1[x_4] = []
         count -= 1
     elif number == 3:
         lst_1.clear()
@@ -61,19 +91,20 @@ def deleat_slovo(number):
         lst_1 = [[] for i in range(x)]  # таблица слов
         lst_2 = ['' for i in range(x)]  # таблица ключей
         print(lst_1, lst_2, sep='\n')
+        count = 0
 
 
 while True:
     choice_variant = (input(
-        'Выбор номера задачи:\n1)Добавить слово\n2)Вывести слово по ключу'
+        'Выбор номера задачи:\n1)Добавить слово\n2)Вывести слово'
         '\n3)вывести всю таблицу\n4)удалить слово\n0)закончить выполнение\nчто делаем? - '))
     if choice_variant == '1':
         slovo_2 = input()
         dobavit_slovo(slovo_2)
         continue
     elif choice_variant == '2':
-        key = int(input('номер который будем выводить - '))
-        vivesti_slovo(key)
+        x_2 = int(input('ищем по слову или ключу - 1)Ключ 2)Слово -'))
+        vivesti_slovo(x_2)
         continue
     elif choice_variant == '3':
         vivesti_vsu_tabel()
